c1b27e08e39dba8520a3f00b0b25f9324f641d1d
Akhlakur Rahman <ssrmm2000@gmail.com>
2024-12-23 00:03:31 +0600

    Admin Panel added

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from jobs.models import *
 # Register your models here.
+
+admin.site.register(user)
+admin.site.register(job_seeker)
+admin.site.register(skills)
+admin.site.register(company)
+admin.site.register(jobs)
+admin.site.register(apply)
